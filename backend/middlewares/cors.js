2e6f96c4fb1c6318f481281c76cb970f3aa90705
Ruslan Bestaev <ruslanbestaev77@yandex.ru>
2022-01-31 18:58:36 +0300

    Подправил cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,6 +7,7 @@ const allowedCors = [
 
 // const requestHeaders = req.headers['access-control-request-headers'];
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
@@ -15,12 +16,13 @@ module.exports = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
   }
   next();
 };
